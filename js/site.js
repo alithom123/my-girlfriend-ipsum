906dc3f39a996b911759ea4f035399240d739101
Ali Thompson <alithom123@gmail.com>
2020-01-23 20:05:07 -0600

    took some files from some other lorem ipsum generators and because they used jquery I also added jquery.  I can remove jquery later if I want to rewrite it.

---
+++ js/site.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    function generateIpsum(paragraphCount, sentenceCount) {
+
+        console.log(`running generateIpsum`);
+        $.getJSON("sentences.json",
+
+            function (data) {
+                var arr = _.shuffle(data);
+                var output = '';
+                for (var p = 0; p < paragraphCount; p++) {
+                    var firstSentences = _.first(arr, sentenceCount);
+                    arr = _.rest(arr, sentenceCount);
+                    output = output + "<p>" + firstSentences.join(" ") + "</p>";
+                }
+                $('.text').html(output);
+            });
+
+    }
+
+    function toggleIpsum(selector, paragraphCount, sentenceCount) {
+        console.log(`running toggleIpsum`);
+        $(selector).on('click', function () {
+            generateIpsum(paragraphCount, sentenceCount);
+        })
+    }
+
+    toggleIpsum('#reload', 5, 5);
+    generateIpsum(5, 5);
+
+});
